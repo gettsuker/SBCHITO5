8160c69443fc623b1ec3a0a9f5e86b705b601767
Ángel Ronaldo Hernando Reyes <37049897+gettsuker@users.noreply.github.com>
2022-12-04 19:43:15 +0100

    Add files via upload
    ARchivo de credentials modificado para poder ser utilizado con nuestro Thingsboard y nuestro BOT, el token del bot ha sido modificado por seguridad.

---
+++ credentials example.h
@@ -0,0 +1,19 @@
+#ifndef CREDENTIALS_HPP
+#define CREDENTIALS_HPP
+
+#define UNIVERSITY "Sbc22-t-06"
+
+//wifi credentials
+
+#define WIFI_NAME             "your wifi ssid"
+#define WIFI_PASSWORD       "your wifi password"
+
+//thingsboard credentials
+
+#define TOKEN   "4U9B75BMy4rz6zcC7Ozb" //TeaSpike PHW 1
+#define THINGSBOARD_SERVER   "demo.thingsboard.io"
+
+//telegram
+
+#define tokenBot "********************************************************"//TokenBot eliminado por seguridad, si quieres saber cual es nuestro TokenBot preguntanos Leo.
+#endif
